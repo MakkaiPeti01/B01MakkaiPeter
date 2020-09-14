d367a4163b42afdfd0a3dfcc4d6b79d78c3f966a
MakkaiPeti01 <PetyaPower@gmail.com>
2020-09-14 11:24:16 +0200

    minimum

--- Program.cs
+++ Program.cs
@@ -9,8 +9,34 @@ namespace B01MakkaiPeter
 {
     class Program
     {
+        static int[] adat = new int[1000];
+        static void Beolvasas()
+        {
+            StreamReader olvas = new StreamReader("adatok.dat");
+            string[] sor = olvas.ReadLine().Split();
+            for (int i = 0; i < adat.Length; i++)
+            {
+                //adat[i] = int.Parse(sor[i]);
+                adat[i] = 3*int.Parse(sor[0]);           
+                Console.WriteLine(adat[i]);
+            }
+        }
+        static int Minimumertek()
+        {
+            int min = adat[0];
+            for (int i = 0; i < adat.Length; i++)
+            {
+                if (min<adat[i])
+                {
+                    min = adat[i];
+                }
+            }
+            return min;
+        }
         static void Main(string[] args)
         {
+            Beolvasas();
+            Console.ReadKey();
         }
     }
 }
